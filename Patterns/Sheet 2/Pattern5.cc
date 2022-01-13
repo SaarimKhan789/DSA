cc966f0435120b289abd57b97ebb852a6e9d4bf8
Saarim Khan <saarimkhan515@gmail.com>
2022-01-13 12:35:53 +0530

    Taking Input n

--- Patterns/Sheet 2/Pattern5.cc
+++ Patterns/Sheet 2/Pattern5.cc
@@ -3,12 +3,14 @@ using namespace std;
 
 int main(){
 
+    int n;
+    cin>>n;
 
-    for(int i=1;i<=7;i++){
+    for(int i=1;i<=n;i++){
 
-        for(int j=1;j<=7;j++){
+        for(int j=1;j<=n;j++){
 
-            if(i==1 || j==1 || i==7 || j==7 || (i==j) || (i+j)==8){
+            if(i==1 || j==1 || i==n || j==n || (i==j) || (i+j)==n+1){
                 cout<<"*";
             }
             else{
